1a7db8b61b4d7a73c475513862cd95e95f73059d
BBeerBear <xiongx659@gmail.com>
2025-03-04 10:42:58 -0500

    docker-compose & data model improvements

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 - docker build -t rest-apis-flask-python
 - docker run -d -p 5000:5000 rest-apis-flask-python
+- docker compose:
+  - docker compose up
+  - docker compose up --build --force-recreate --no-deps web
--- app.py
+++ app.py
@@ -1,47 +1,61 @@
+import uuid
+
 from flask import Flask, request
 
-stores = [{"name": "My Store", "items": [{"name": "my item", "price": 15.99}]}]
+from db import items, stores
 
 app = Flask(__name__)
 
 
 @app.get("/store")
 def get_stores():
-    return {"stores": stores}
+    return {"stores": list(stores.values)}
 
 
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    new_store = {**store_data, id: store_id}
+    stores[store_id] = new_store
     return new_store, 201
 
 
 app.post("/store/<string:name>/item")
 
 
-def create_item(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
-            return new_item
-    return {"message": "Store not found"}, 404
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "Store not found"}, 404
+    item_id = uuid.uuid4().hex
+    new_item = {**item_data, "id": item_id}
+    items[item_id] = new_item
+    return new_item
 
 
 @app.get("/store/<string:name>")
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store
-    return {"message": "Store not found"}, 404
+def get_store(store_id):
+    try:
+        # Here you might also want to add the items in this store
+        # We'll do that later on in the course
+        return stores[store_id]
+    except KeyError:
+        return {"message": "Store not found"}, 404
 
 
-@app.get("/store/<string:name>/item")
-def get_item_in_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return {"items": store["items"]}
-    return {"message": "Store not found"}, 404
+@app.get("/item/<string:item_id>")
+def get_item_in_store(item_id):
+    # for store in stores:
+    #     if store["name"] == name:
+    #         return {"items": store["items"]}
+    # return {"message": "Store not found"}, 404
+    try:
+        return items[item_id]
+    except KeyError:
+        return {"message": "Item not found"}, 404
+
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
