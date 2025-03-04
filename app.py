2522a9bc6dcbdec61ac53a2e85badd9f85baad94
BBeerBear <xiongx659@gmail.com>
2025-03-04 10:59:19 -0500

    improve restapis with flask-smorest abort

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import uuid
 
 from flask import Flask, request
+from flask_smorest import abort
 
 from db import items, stores
 
@@ -15,33 +16,51 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        return abort(
+            400, message="Bad request. Ensure 'name' is included in the JSON payload."
+        )
+    for store in stores.values:
+        if store_data["name"] == store["name"]:
+            return abort(400, message=f"Store {store_data['name']}  already exists.")
     store_id = uuid.uuid4().hex
     new_store = {**store_data, id: store_id}
     stores[store_id] = new_store
     return new_store, 201
 
 
-app.post("/store/<string:name>/item")
-
-
+@app.post("/store/<string:name>/item")
 def create_item():
     item_data = request.get_json()
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        return abort(404, message="Store not found")
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        return abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
+        )
+    for item in items:
+        if item_data["name"] == item["name"]:
+            return abort(400, message=f"Item {item_data['name']} already exists.")
+
     item_id = uuid.uuid4().hex
     new_item = {**item_data, "id": item_id}
     items[item_id] = new_item
     return new_item
 
 
-@app.get("/store/<string:name>")
+@app.get("/store/<string:store_id>")
 def get_store(store_id):
     try:
         # Here you might also want to add the items in this store
         # We'll do that later on in the course
         return stores[store_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found.")
 
 
 @app.get("/item/<string:item_id>")
@@ -53,7 +72,7 @@ def get_item_in_store(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": "Item not found"}, 404
+        abort(404, message="Item not found.")
 
 
 @app.get("/item")
