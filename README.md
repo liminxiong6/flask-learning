70b30b1e44697cd6736c01c79052f3706ae84e73
BBeerBear <xiongx659@gmail.com>
2025-03-07 11:44:59 -0500

    one to many relationships

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 - docker compose:
   - docker compose up
   - docker compose up --build --force-recreate --no-deps web
+- We'll be using a relational database for data storage, and there are many different options: SQLite, MySQL, PostgreSQL, and others. ORM, which can take Python objects and turn them into database rows.
+- ORM library (SQLAlchemy) helps us with many potential issues with using SQL, such as:
+  - Multi-threading support
+  - Handling creating the tables and defining the rows
+  - Database migrations (with help of another library, Alembic)
+  - Like mentioned, it makes the code cleaner, simpler, and shorter
+- What is Flask-SQLAlchemy?
+  - SQLAlchemy is the ORM library, that helps map Python classes to database tables and columns, and turns Python objects of those classes into specific rows.
+  - Flask-SQLAlchemy is a Flask extension which helps connect SQLAlchemy to Flask apps.
--- schemas.py
+++ schemas.py
@@ -1,11 +1,10 @@
 from marshmallow import Schema, fields
 
 
-class ItemSchema(Schema):
+class PlainItemSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
-    store_id = fields.Str(required=True)
 
 
 class ItemUpdateSchema(Schema):
@@ -13,6 +12,20 @@ class ItemUpdateSchema(Schema):
     price = fields.Float()
 
 
-class StoreSchema(Schema):
+class PlainStoreSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
+
+
+class ItemSchema(Schema):
+    store_id = fields.Int(
+        required=True, load_only=True
+    )  # Only data from the client(whoever call the api, json) will be passed in the store_id
+    store = fields.Nested(
+        PlainStoreSchema(),
+        dump_only=True,  # Used only when returning data to the client
+    )
+
+
+class StoreSchema(Schema):
+    items = fields.List(fields.Nested(PlainItemSchema(), dump_only=True))
