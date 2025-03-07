c03761c8d218b2f3f1654ac2a76a322542e540bd
BBeerBear <xiongx659@gmail.com>
2025-03-07 15:23:26 -0500

    Get models by ID from the database using SQLAlchemy

--- resources/item.py
+++ resources/item.py
@@ -1,10 +1,10 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-
-from schemas import ItemSchema, ItemUpdateSchema
-from models import ItemModel
 from sqlalchemy.exc import SQLAlchemyError
+
 from db import db
+from models import ItemModel
+from schemas import ItemSchema, ItemUpdateSchema
 
 blp = Blueprint("items", __name__, "Operation on items")
 
@@ -14,28 +14,22 @@ class Item(MethodView):
     @blp.response(200, ItemSchema)
     def get(self, item_id):
         try:
-            return items[item_id]
+            item = ItemModel.query.get_or_404(item_id)
+            return item
         except KeyError:
             abort(404, message="Item not found.")
 
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {"message": "Item deleted."}
-        except KeyError:
-            abort(404, message="Item not found")
+        item = ItemModel.query.get_or_404(item_id)
+        raise NotImplementedError("Deleting an item is not implemented.")
 
     # Be careful here since we've now got item_data and item_id.
     # The URL arguments come in at the end. The injected arguments are passed first,
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        try:
-            item = items[item_id]
-            item |= item_data
-            return item
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        raise NotImplementedError("Deleting an item is not implemented.")
 
 
 @blp.route("/item")
--- resources/store.py
+++ resources/store.py
@@ -12,17 +12,12 @@ blp = Blueprint("stores", __name__, description="Operation on stores")
 class Store(MethodView):
     @blp.response(200, StoreSchema)
     def get(self, store_id):
-        try:
-            return stores[store_id]
-        except KeyError:
-            abort(404, message="Store not found.")
+        store = StoreModel.query.get_or_404(store_id)
+        return store
 
     def delete(self, store_id):
-        try:
-            del stores[store_id]
-            return {"message": "Store deleted."}
-        except KeyError:
-            abort(404, message="Store not found.")
+        store = StoreModel.query.get_or_404(store_id)
+        raise NotImplementedError("Deleting a store is not implemented.")
 
 
 @blp.route("/store")
