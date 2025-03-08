1ad468c05e02751ca0cb9879be05f0f1a96871f4
BBeerBear <xiongx659@gmail.com>
2025-03-08 11:24:48 -0500

    add a register endpoint

---
+++ resources/user.py
@@ -0,0 +1,25 @@
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from passlib.hash import pbkdf2_sha256
+
+from db import db
+from models import UserModel
+from schemas import UserSchame
+
+blp = Blueprint("users", __name__, description="Operation on users")
+
+
+@blp.route("/register")
+class UserRegister(MethodView):
+    @blp.arguments(UserSchame)
+    def post(self, user_data):
+        if UserModel.query.filter(UserModel.username == user_data["username"]).first():
+            abort(409, message="A user with that username already exists.")
+
+        user = UserModel(
+            username=user_data["username"],
+            password=pbkdf2_sha256.hash(user_data["password"]),
+        )
+        db.session.add(user)
+        db.session.commit()
+        return {"message": "User created successfully"}
--- schemas.py
+++ schemas.py
@@ -54,5 +54,5 @@ class TagAndItemSchema(Schema):
 
 class UserSchame(Schema):
     id = fields.Int(dump_only=True)
-    name = fields.Str(required=True)
+    username = fields.Str(required=True)
     password = fields.Str(required=True, load_only=True)
