2288e680036f54d1e85327e4cdb338c40ce7f648
BBeerBear <xiongx659@gmail.com>
2025-03-09 23:04:30 -0400

    Protect endpoints by requiring a JWT

--- resources/item.py
+++ resources/item.py
@@ -1,7 +1,7 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError
-
+from flask_jwt_extended import jwt_required
 from db import db
 from models import ItemModel
 from schemas import ItemSchema, ItemUpdateSchema
@@ -11,6 +11,7 @@ blp = Blueprint("items", __name__, "Operation on items")
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
+    @jwt_required()
     @blp.response(200, ItemSchema)
     def get(self, item_id):
         try:
@@ -19,6 +20,7 @@ class Item(MethodView):
         except KeyError:
             abort(404, message="Item not found.")
 
+    @jwt_required()
     def delete(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         db.session.delete(item)
@@ -44,10 +46,12 @@ class Item(MethodView):
 
 @blp.route("/item")
 class ItemList(MethodView):
+    @jwt_required()
     @blp.response(200, ItemSchema(many=True))
     def get(self):
         return ItemModel.query.all()
 
+    @jwt_required()
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
