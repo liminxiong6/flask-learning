e5bbea3582d4ec1790b5706b94c78f26cae29148
BBeerBear <xiongx659@gmail.com>
2025-03-12 19:48:45 -0400

    Token refreshing with Flask-JWT-Extended

--- resources/user.py
+++ resources/user.py
@@ -1,7 +1,13 @@
 from datetime import datetime, timezone
 
 from flask.views import MethodView
-from flask_jwt_extended import create_access_token, get_jwt, jwt_required
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    get_jwt,
+    get_jwt_identity,
+    jwt_required,
+)
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
 
@@ -59,8 +65,9 @@ class UserLogin(MethodView):
         ).first()
 
         if user and pbkdf2_sha256.verify(user_data["password"], user.password):
-            access_token = create_access_token(identity=str(user.id))
-            return {"access_token": access_token}, 200
+            access_token = create_access_token(identity=str(user.id), fresh=True)
+            refresh_token = create_refresh_token(str(user.id))
+            return {"access_token": access_token, "refresh_token": refresh_token}, 200
         abort(401, message="Invalid credentials.")
 
 
@@ -76,3 +83,19 @@ class UserLogout(MethodView):
         db.session.commit()
 
         return {"message": "Successfully logged out"}, 200
+
+
+@blp.route("/refresh")
+class TokenRefresh(MethodView):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        # Make it clear that when to add the refresh token to the blocklist will depend on the app design
+        jwt = get_jwt()
+        jti = jwt["jti"]
+        expires = datetime.fromtimestamp(jwt["exp"], tz=timezone.utc)
+        token = TokenBlocklist(jti=jti, expires_at=expires)
+        db.session.add(token)
+        db.session.commit()
+        return {"access_token": new_token}, 200
